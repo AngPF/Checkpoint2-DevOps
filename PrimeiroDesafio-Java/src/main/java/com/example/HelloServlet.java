9e76119917e6a2310cf4b9643f1d434fe51bcd4a
Angelo <129402181+AngPF@users.noreply.github.com>
2024-04-24 22:50:55 -0300

    Primeiro Desafio - Tecnologia Java

---
+++ PrimeiroDesafio-Java/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Deploy efetuado com sucesso no Servidor Tomcat 10!</h2>");
+        out.println("</body></html>");
+        out.close();
+    }
+
+}
---
+++ PrimeiroDesafio-Java/target/checkPoint/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Deploy efetuado com sucesso no Servidor Tomcat 10!</h2>
+</body>
+</html>
